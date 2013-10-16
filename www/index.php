cd0f0a5693574b9674ce5d8f17cd4b1627c57515
Bryan Shelton <bryan@sheltonplace.com>
2013-10-16 09:58:02 -0700

    Read jenkins_url from various locations.

--- www/index.php
+++ www/index.php
@@ -2,6 +2,24 @@
 require_once __DIR__.'/../vendor/autoload.php';
 use BitBucket\Hook\Jenkins\GitNotifyCommit;
 
+$jenkins_url = FALSE;
+
+// NOTE: Needs to be moved into a library so it can be tested
+if (isset($_GET['jenkins_url'])) {
+  $jenkins_url = $_GET['jenkins_url'];
+}
+elseif (getenv('JENKINS_URL')) {
+  $jenkins_url = getenv('JENKINS_URL');
+}
+elseif (file_exists(__DIR__.'/../config.json')) {
+  $config = json_decode(file_get_contents(__DIR__.'/../config.json'));
+  if (isset($config->jenkins_url)) {
+    $jenkins_url = $config->jenkins_url;
+  }
+}
+
+print $jenkins_url;
+
 if (isset($_GET['jenkins_url']) && isset($_POST['payload'])) {
   $git_notify = new GitNotifyCommit($_GET['jenkins_url'], $_POST['payload']);
   // Trigger a url hit
